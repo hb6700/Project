c663ad1bb779dd9de82264bc98461940def0fd1f
cktnals16 <cktnals16@gmail.com>
2023-08-17 10:18:06 +0900

    장바구니 삭제

--- src/com/test/java/cart/Cart.java
+++ src/com/test/java/cart/Cart.java
@@ -4,7 +4,13 @@ public class Cart {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("장바구니");
+		System.out.println("장바구니 담기");
+		
+	}
+	
+	public void clear() {
+		
+		System.out.println("장바구니 삭제");
 		
 	}
 	
