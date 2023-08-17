114bd0f0eee668f88b9512e3cb168809bfedd633
chobo9988165 <tjdtl651@naver.com>
2023-08-17 10:49:00 +0900

    카트 패키지 생성

---
+++ src/com/test/java/cart/Cart.java
@@ -0,0 +1,13 @@
+package com.test.java.cart;
+
+public class Cart {
+	
+	public void add() {
+		System.out.println("장바구니 담기");
+	}
+	
+	public void clear() {
+		System.out.println("장바구니 비우기");
+	}
+
+}
